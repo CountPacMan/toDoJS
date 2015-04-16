06e97f88f3ab1477d65912f0274d3b05eb342b4e
Daniel Toader & Tanner Heffner <student@epicodus.com>
2015-04-16 14:05:23 -0700

    fix most of clicker logic but fail by falling short

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,24 @@
 var currentCategory;
 var allCategories = [];
 
-function addTaskClicker(task) {
-  $("#show-completed").show();
+function addTaskClicker(task, child) {
   task.done = true;
-  $(this).parent().remove();
-  $(".complete").last().click(addCompletedTaskClicker(task));
+  child.parent().remove();
+  $(".task-completed").append("<li><span class='complete'>" + task.name + "</span></li>");
+  child = $(".complete").last();
+  $(".complete").last().click(function() {
+    addCompletedTaskClicker(task, child);
+  });
 }
 
-function addCompletedTaskClicker(task) {
+function addCompletedTaskClicker(task, child) {
   task.done = false;
-  $(this).parent().remove();
-  $(".task").last().click(addTaskClicker(task));
+  child.parent().remove();
+  $("ul#tasks").append("<li><span class='task'>" + task.name + "</span></li>");
+  child = $(".task").last();
+  $(".task").last().click(function() {
+    addTaskClicker(task, child);
+  });
 }
 
 jQuery(document).ready(function() {
@@ -29,6 +36,8 @@ jQuery(document).ready(function() {
         $("#tasks").text("");
         $(".task-completed").text("");
         $("#add-task").show();
+        $("#show-completed").show();
+
         currentCategory = $(this).text();
         var index;
         for (var i in allCategories) {
@@ -37,13 +46,20 @@ jQuery(document).ready(function() {
           }
         }
         var ourCategory = allCategories[index];
+
         ourCategory.tasks.forEach(function(task) {
           if (!task.done) {
             $("ul#tasks").append("<li><span class='task'>" + task.name + "</span></li>");
-            $(".task").last().click(addTaskClicker(task));
+            var child = $(".task").last();
+            $(".task").last().click(function() {
+              addTaskClicker(task, child);
+            });
           } else {
             $(".task-completed").append("<li><span class='complete'>" + task.name + "</span></li>");
-            $(".complete").last().click(addCompletedTaskClicker(task));
+            var child = $(".complete").last();
+            $(".complete").last().click(function() {
+              addCompletedTaskClicker(task, child);
+            });
           }
         });
       });
@@ -51,13 +67,8 @@ jQuery(document).ready(function() {
     clicker();
   });
 
-
-
-
   $("#add-task").submit(function(event) {
     event.preventDefault();
-    // loop through and append all contacts of the clicked categor
-
 
     $(".task-list").show();
 
@@ -71,26 +82,12 @@ jQuery(document).ready(function() {
     }
     var ourCategory = allCategories[index];
     ourCategory.tasks.push(newTask);
-
     $("ul#tasks").append("<li><span class='task'>" + newTask.name + "</span></li>");
 
     $("#task").val("");
-
-    $(".task").last().click(addTaskClicker(task));
-    // function clicker() {
-    //   $(".task").last().click(function(){
-    //     $("#show-completed").show();
-    //     $(".task-completed").append("<li><span class='complete'>" + newTask.name + "</span></li>");
-    //     newTask.done = true;
-    //     $(this).parent().remove();
-    //     $(".complete").last().click(function() {
-    //       $("ul#tasks").append("<li><span class='task'>" + newTask.name + "</span></li>");
-    //       newTask.done = false;
-    //       $(this).parent().remove();
-    //       clicker();
-    //     });
-    //   });
-    // }
-    // clicker();
+    var child = $(".task").last();
+    $(".task").last().click(function() {
+      addTaskClicker(task, child);
+    });
   });
 });

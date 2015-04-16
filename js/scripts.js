82f1cc9ea69058740bba2790001870973d324229
Daniel Toader & Tanner Heffner <student@epicodus.com>
2015-04-16 15:10:50 -0700

    refactor

--- js/scripts.js
+++ js/scripts.js
@@ -31,40 +31,37 @@ jQuery(document).ready(function() {
     $("#categories").append("<li><span class='category'>" + newCategory.name + "</span></li>");
     $("#category").val("");
 
-    function clicker() {
-      $(".category").last().click(function() {
-        $("#tasks").text("");
-        $(".task-completed").text("");
-        $("#add-task").show();
-        $("#show-completed").show();
+    $(".category").last().click(function() {
+      $("#tasks").text("");
+      $(".task-completed").text("");
+      $("#add-task").show();
+      $("#show-completed").show();
 
-        currentCategory = $(this).text();
-        var index;
-        for (var i in allCategories) {
-          if (allCategories[i].name === currentCategory) {
-            index = i;
-          }
+      currentCategory = $(this).text();
+      var index;
+      for (var i in allCategories) {
+        if (allCategories[i].name === currentCategory) {
+          index = i;
         }
-        var ourCategory = allCategories[index];
+      }
+      var ourCategory = allCategories[index];
 
-        ourCategory.tasks.forEach(function(task) {
-          if (!task.done) {
-            $("ul#tasks").append("<li><span class='task'>" + task.name + "</span></li>");
-            var child = $(".task").last();
-            $(".task").last().click(function() {
-              addTaskClicker(task, child);
-            });
-          } else {
-            $(".task-completed").append("<li><span class='complete'>" + task.name + "</span></li>");
-            var child = $(".complete").last();
-            $(".complete").last().click(function() {
-              addCompletedTaskClicker(task, child);
-            });
-          }
-        });
+      ourCategory.tasks.forEach(function(task) {
+        if (!task.done) {
+          $("ul#tasks").append("<li><span class='task'>" + task.name + "</span></li>");
+          var child = $(".task").last();
+          $(".task").last().click(function() {
+            addTaskClicker(task, child);
+          });
+        } else {
+          $(".task-completed").append("<li><span class='complete'>" + task.name + "</span></li>");
+          var child = $(".complete").last();
+          $(".complete").last().click(function() {
+            addCompletedTaskClicker(task, child);
+          });
+        }
       });
-    }
-    clicker();
+    });
   });
 
   $("#add-task").submit(function(event) {
